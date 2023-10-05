d553713ccc53a8fa17554b5ec101ad233eca02ac
Hugo <hugovaya12@gmail.com>
2023-10-05 08:59:55 +0200

    Primer comit

---
+++ WhatsDAMLolliPop/app/src/main/java/com/example/whatsdamlolipop/MessageWindowsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.whatsdamlolipop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MessageWindowsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message_windows)
+    }
+}
