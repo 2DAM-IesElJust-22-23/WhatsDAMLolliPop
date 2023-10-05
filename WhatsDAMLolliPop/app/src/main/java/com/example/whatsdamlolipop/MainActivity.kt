1866c78029935cfa8ea76e4a668f966cf8b63e83
Hugo <hugovaya12@gmail.com>
2023-10-05 10:27:18 +0200

    Segon comit

--- WhatsDAMLolliPop/app/src/main/java/com/example/whatsdamlolipop/MainActivity.kt
+++ WhatsDAMLolliPop/app/src/main/java/com/example/whatsdamlolipop/MainActivity.kt
@@ -1,11 +1,50 @@
 package com.example.whatsdamlolipop
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.InetAddresses
+import android.os.Build
 import android.os.Bundle
-
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whatsdamlolipop.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private var nom = ""
+    private var server = ""
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState != null){
+
+            nom = savedInstanceState.getString("nom","")
+            server = savedInstanceState.getString("server","")
+
+        }
+
+        binding.buttonConnect.setOnClickListener  {
+
+            val intent = Intent(this, MessageWindowsActivity::class.java)
+
+            nom = binding.nickNameText.text.toString()
+            server = binding.serverAddressText.text.toString()
+
+            if(nom.isNotEmpty() && InetAddresses.isNumericAddress(server)){
+                
+                intent.putExtra("NICKNAME_KEY", nom)
+                intent.putExtra("IPSERVER", server)
+                startActivity(intent)
+
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle){
+        super.onSaveInstanceState(outState)
+        outState.putString("nom", nom)
+        outState.putString("server", server)
     }
 }
--- WhatsDAMLolliPop/app/src/main/java/com/example/whatsdamlolipop/MessageWindowsActivity.kt
+++ WhatsDAMLolliPop/app/src/main/java/com/example/whatsdamlolipop/MessageWindowsActivity.kt
@@ -1,11 +1,33 @@
 package com.example.whatsdamlolipop
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whatsdamlolipop.databinding.ActivityMessageWindowsBinding
 
 class MessageWindowsActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_message_windows)
+
+    private lateinit var binding: ActivityMessageWindowsBinding
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            binding = ActivityMessageWindowsBinding.inflate(layoutInflater)
+            setContentView(binding.root)
+
+            val message = binding.MessageText
+            val textV = binding.connectionInfoTextView
+            val sendM = binding.sendMessage
+
+
+            val nickname = intent.getStringExtra("NICKNAME_KEY")
+            val server = intent.getStringExtra("IPSERVER")
+
+            textV.text = "Conectat a $nickname com a $server"
+
+            sendM.setOnClickListener(){
+                message.text.clear()
+            }
+
+        }
+
     }
-}
+
